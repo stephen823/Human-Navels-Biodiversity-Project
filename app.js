77b1f903bc37eba9928d773ddbe53bb3748e544b
Stephen Zhang <stephenzhang95@hotmail.com>
2020-05-21 01:13:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 //Read data from the JSON file
-d3.json(samples.json).then((imp_data)=> {var data=imp_data});
+d3.json("samples.json").then((imp_data)=> {var data=imp_data});
 console.log(imp_data);
